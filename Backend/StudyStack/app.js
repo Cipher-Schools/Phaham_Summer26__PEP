71e3e86e2c2d0eb0e9254a5d37e08adce58e490f
Phaham Khan <phaham17@gmail.com>
2026-08-02 16:38:09 +0530

    app.js updated

--- Backend/StudyStack/app.js
+++ Backend/StudyStack/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const courseRoutes = require('./routes/courseRoutes');
 const authRoutes = require('./routes/authRoutes');
-const chatRoutes = require('./routes/chatRoutes');
+// const chatRoutes = require('./routes/chatRoutes');
 
 const logger = require('./middlewares/logger');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/courses', courseRoutes);
 app.use('/', authRoutes);
-app.use('/api', chatRoutes);
+// app.use('/api', chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
